10207cc6b807e8a52a0cf8594a9879e0ffb1e226
xiaolong.lxl <xiaolong.lxl@alibaba-inc.com>
2017-08-04 17:55:14 +0800

    add services

--- src/services/employee.js
+++ src/services/employee.js
@@ -5,40 +5,28 @@
 
 'use strict';
 
-const Sequelize = require('sequelize');
-const sq = require('../db').sequelize;
 const employee = require('./model/employee');
-const exam = require('./model/exam');
-const normalize = require('./lib/data-normalize');
 
 module.exports = {
-  create: function* (workerId, nick, name, email, account, tbww) {
-    return normalize(
-      yield employee.create({
-        employeeId: workerId,
-        nick: nick,
-        name: name,
-        email: email,
-        account: account,
-        tbww: tbww
-      })
-    );
+  create: async function(workerId, nick, name, email, account) {
+    return await employee.create({
+      employeeId: workerId,
+      nick: nick,
+      name: name,
+      email: email,
+      account: account
+    });
   },
-  get: function* (workerId) {
-    return normalize(
-      yield employee.findOne({
-        where: {
-          employeeId: workerId
-        }
-      })
-    );
-  },
-  getList: function* () {
-    /*return employee.findAll({
-      order: [
-        // Will order by max(age) DESC
-        [sequelize.fn('max', sequelize.col('eId')), 'DESC']
-      ]
-    });*/
+  /**
+   * 如何获取一个 plain object http://docs.sequelizejs.com/class/lib/model.js~Model.html#instance-method-get
+   * @param workerId
+   * @returns {*}
+   */
+  get: async function(workerId) {
+    return await employee.findOne({
+      where: {
+        employeeId: workerId
+      }
+    });
   }
 };
